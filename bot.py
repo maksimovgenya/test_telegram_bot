47d60176e8c8df0099e206f56758f4e991dcf9d7
Evghenii Maximov <maksimovgenya@gmail.com>
2021-04-25 22:27:50 +0300

    bot_versia_1

---
+++ bot.py
@@ -0,0 +1,39 @@
+# Логируем ошибки
+import logging
+
+# Логируем токен телеграм
+import settings
+
+# Подключаемся
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+
+# Файл для логирования
+logging.basicConfig(format='%(asctime)s - %(levelname)s  - %(message)s',
+					level=logging.INFO,
+					filename='bot.log'
+					)
+
+def start_bot(update: Updater, context: CallbackContext):
+	mytext = """Приветствуем тебя {}
+
+	Это частный чат-бот CrowdWiz для Евгения и Виталия""".format(update.message.chat.first_name)
+	update.message.reply_text(mytext)
+
+def chat(update: Updater, context: CallbackContext):
+	text = update.message.text
+	logging.info(text)
+
+	update.message.reply_text(text)
+
+def main():
+	updtr = Updater(settings.TOKEN_TELEGRAMM)
+
+	updtr.dispatcher.add_handler(CommandHandler("start", start_bot))
+	updtr.dispatcher.add_handler(MessageHandler(Filters.text, chat))
+
+	updtr.start_polling()
+	updtr.idle()
+
+if __name__ == "__main__":
+	logging.info('Bot started!')
+	main()
