6b43f048beae997bec1d4a95f3530d1e05c49d19
maksimovgenya <83228367+maksimovgenya@users.noreply.github.com>
2021-04-26 18:48:28 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -17,6 +17,7 @@ def start_bot(update: Updater, context: CallbackContext):
 	mytext = """Приветствуем тебя {}
 
 	Это частный чат-бот CrowdWiz для Евгения и Виталия""".format(update.message.chat.first_name)
+	logging.info('User {} press button /start'.format(update.message.chat.username))
 	
 	update.message.reply_text(mytext)
 
@@ -27,8 +28,9 @@ def chat(update: Updater, context: CallbackContext):
 	update.message.reply_text(text)
 
 def main():
-	updtr = Updater(settings.TOKEN_TELEGRAMM)
+	updtr = Updater('1766791115:AAG9KiAlVOR67iDx3Qyq4pV8TfOJO9KLLNw')
 
+	#'1766791115:AAG9KiAlVOR67iDx3Qyq4pV8TfOJO9KLLNw'
 	#settings.TOKEN_TELEGRAMM
 
 	updtr.dispatcher.add_handler(CommandHandler("start", start_bot))
